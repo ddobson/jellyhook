d9722219945cca518f261680bddffcf486c2ec25
ddobson <ddobson445@gmail.com>
2025-04-08 01:14:16 -0400

    Fix linting

--- api/src/api/logging.py
+++ api/src/api/logging.py
@@ -5,6 +5,7 @@ import sys
 from flask import Flask, request
 from loguru import logger
 
+from api import config
 from api.config import Env
 
 # Completely silence specific libraries
@@ -62,6 +63,8 @@ def init_app(app: Flask):
     Args:
         app: Flask application instance
     """
+    is_production = config.ENV == Env.PRODUCTION
+
     # Remove default logger
     logger.remove()
 
@@ -75,7 +78,7 @@ def init_app(app: Flask):
 
     # Add console handler
     logger.configure(patcher=_format_json_message)
-    logger.add(sys.stdout, format=log_format, level="INFO", colorize=not is_production)
+    logger.add(sys.stdout, format=log_format, level="INFO", colorize=True)
 
     # Disable all handlers for the silenced loggers
     for logger_name in SILENCED_LOGGERS:
